366d70fe49901317ba3dc053b54a4e21fff4aa60
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-04-15 23:05:51 +1000

    new api request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var searchBtn = document.getElementById("searchBtn");
-var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?&q=city&cnt=5&exclude=hourly,daily&appid=5ee2ef72f8bcd5f71cb4cc0992822390&units=imperial";
+var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?q=city&exclude=hourly,daily&appid=5ee2ef72f8bcd5f71cb4cc0992822390&units=imperial";
 
 var cityFormEl = document.querySelector('#city-form');
 var userInputEl = document.querySelector('#userSearch');
@@ -13,9 +13,9 @@ var formSubmitHandler = function (event) {
     var userSearch = userInputEl.value.trim();
   
     if (userSearch) {
-      getApi("https://api.openweathermap.org/data/2.5/forecast?q=" + userSearch + "&cnt=5&exclude=hourly,daily&appid=5ee2ef72f8bcd5f71cb4cc0992822390&units=imperial");
+      getApi("https://api.openweathermap.org/data/2.5/onecall?q=" + userSearch + "&exclude=hourly,daily&appid=5ee2ef72f8bcd5f71cb4cc0992822390&units=imperial");
         
-      cardHeaderEl.textContent = '';
+    //   cardHeaderEl.textContent = '';
       weatherContainerEl.textContent = '';
       userInputEl.value = '';
     } else {
@@ -30,7 +30,8 @@ function getApi(requestUrl) {
         if (response.ok){
             response.json().then(function (data) {
                 console.log(data);
-                weatherContainerEl.innerHTML = "<h1>" + data.city.name + "</h1><img>" + data.list[0].weather[0].icon + "</img><h3>Date: " 
+                weatherContainerEl.innerHTML = "<h1>" + data.city.name + "</h1><img src=http://openweathermap.org/img/w/" + 
+                data.list[0].weather[0].icon + ".png alt='Icon depicting current weather' width='50' height='50'><h3>Date: " 
                 + data.list[0].dt_txt + "</h3><h3>Temp: " + data.list[0].main.temp + 
                 "</h3><h3>Humidity: " + data.list[0].main.humidity + "</h3><h3>Wind Speed: " 
                 + data.list[0].wind.speed + "</h3>";
@@ -67,8 +68,6 @@ function getApi(requestUrl) {
             var reportEl = document.createElement("div");
             reportEl.classList =  'list-item flex-row justify-space-between align-center';
 
-            
-
             // 
             reportEl.appendChild();
             // 
@@ -78,6 +77,42 @@ function getApi(requestUrl) {
     };
 
 cityFormEl.addEventListener('submit', formSubmitHandler);
+
+
+// ============================================
+// function getApi(requestUrl) {
+//     fetch(requestUrl)
+//     .then(function (response) {
+//         if (response.ok){
+//             response.json().then(function (data) {
+//                 console.log(data);
+//                 weatherContainerEl.innerHTML = "<h1>" + data.city.name + "</h1><img src=http://openweathermap.org/img/w/" + 
+//                 data.list[0].weather[0].icon + ".png alt='Icon depicting current weather' width='50' height='50'><h3>Date: " 
+//                 + data.list[0].dt_txt + "</h3><h3>Temp: " + data.list[0].main.temp + 
+//                 "</h3><h3>Humidity: " + data.list[0].main.humidity + "</h3><h3>Wind Speed: " 
+//                 + data.list[0].wind.speed + "</h3>";
+
+//                 console.log("Temp:", data.list[0].main.temp,
+//                 "Humidity:", data.list[0].main.humidity,
+//                 "Wind speed:", data.list[0].wind.speed
+//                 );
+//                 displayReport(data);
+//             });
+//         }else {
+//             alert("error: " + response.statusText);
+//         }
+//     })
+//         .catch(function (error) {
+//             alert("UPPPSSS");
+//         });
+//         // console.log(response);
+//         // return response.json();       
+//     };
+
+
+// var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?&q=city&cnt=5&exclude=hourly,daily&appid=5ee2ef72f8bcd5f71cb4cc0992822390&units=imperial";
+// getApi("https://api.openweathermap.org/data/2.5/forecast?q=" + userSearch + "&cnt=5&exclude=hourly,daily&appid=5ee2ef72f8bcd5f71cb4cc0992822390&units=imperial");
+// =============================================
 
 
 // var repoSearchTerm = document.querySelector('#repo-search-term');
