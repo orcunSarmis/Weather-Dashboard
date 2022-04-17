8d11cb2bee8c00eb9ef7e19c937832e7c16807cc
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-04-17 23:13:42 +1000

    new loc al storage lines added

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,14 +72,19 @@ function getApi(requestUrl) {
                 + curTemp + "</h3><h3>Wind Speed: " + curWind + "</h3><h3>Humidity: " + curHumid + "</h3><h3>UV Index: " + curUV + "</h3>";
 
                 // Add user input city name in local storage.
-                localStorage.setItem("city", JSON.stringify({cityName}));
-                var city = localStorage.getItem("cityName");
+                var allcities = JSON.parse(localStorage.getItem("cities")) || [];
+                allcities.push(userInputEl.value);
+                userInputEl.value = '';
+                localStorage.setItem("cities", allcities);
+                
 
-                searchBtn.addEventListener('click', () => {
-                    city.push(userInputEl.value);
-                    userInputEl.value= '';
-                    console.log( value);
-                })
+                // localStorage.setItem("city", JSON.stringify({city}));
+                // var city = localStorage.getItem("cityName");
+
+                // searchBtn.addEventListener('click', () => {
+                //     city.push(userInputEl.value);
+                //     console.log( value);
+                // })
                 // This line call back city name from local storage.
                 // var memory = document.getElementById("cityStore").textContent = city.value;
                 displayReport(data);
